ebf6730809a92c8c89488a4320648409f8cd3c2d
温振刚 <opnicorobin@hotmail.com>
2020-03-01 16:22:17 +0800

    添加项目文件。

---
+++ MyFirstGitProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我的第一个git项目");
+            Console.ReadLine();
+        }
+    }
+}
