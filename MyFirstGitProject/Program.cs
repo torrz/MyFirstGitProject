670691f7b889a0f69a470c507331d9ed89c3f6e6
温振刚 <opnicorobin@hotmail.com>
2020-03-01 16:55:51 +0800

    添加标记版本号

--- MyFirstGitProject/Program.cs
+++ MyFirstGitProject/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MyFirstGitProject
 {
+    /// <summary>
+    /// 开发版本2.0.0.0
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
