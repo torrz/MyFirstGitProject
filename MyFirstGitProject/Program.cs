e59a2db1a33e935a1b819d0c26ca93a5483e0b4d
温振刚 <opnicorobin@hotmail.com>
2020-03-01 16:44:29 +0800

    修改main方法

--- MyFirstGitProject/Program.cs
+++ MyFirstGitProject/Program.cs
@@ -10,7 +10,7 @@ namespace MyFirstGitProject
     {
         static void Main(string[] args)
         {
-            var Tom = new Cat() { Id = 1, Name = "Tom" };
+            var Tom = new Cat() { Id = 1, Name = "Tom",Extend="英短",Color="蓝色" };
             Console.WriteLine(Tom.ToString());
             Console.ReadLine();
         }
